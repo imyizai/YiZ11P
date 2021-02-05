eca6f125c54dc4b125df7f55278d04bd76d1519e
imyizai <1030662977@qq.com>
2021-02-05 21:50:17 +0800

    自定义导航和自定义搜索组件

--- miniprogram/pages/find/find.js
+++ miniprogram/pages/find/find.js
@@ -1,22 +1,17 @@
-
+let keyword = '' //搜索关键字
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-     cards:[
-       {},{},{},{}
-     ]
+  
+  data: {},
+  onSearch(event) {
+    keyword = event.detail.keyword
+    console.log(keyword)
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad(options) {},
+  onPublish() {
+   wx.navigateTo({
+     url:'../publish/publish',
+   })
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
